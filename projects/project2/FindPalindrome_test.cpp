42f9941c13c8e1e35a3b807a27e0274acd57202b
Alex <awi4@pitt.edu>
2023-02-27 11:10:39 -0500

    FindPalindrome class complete

--- projects/project2/FindPalindrome_test.cpp
+++ projects/project2/FindPalindrome_test.cpp
@@ -20,9 +20,39 @@ TEST_CASE("test recursion", "[FindPalindrome]"){
 	REQUIRE(b.number() == 6);
 }
 
-TEST_CASE("adding a vector", "[FindPalindrome]") {
+TEST_CASE("adding vectors and strings", "[FindPalindrome]") {
 	FindPalindrome b;
-	vector<string> addVec = {"a", "AA", "AaA"};
-	REQUIRE(b.add(addVec));
+	vector<string> addVec;
+	addVec.push_back("a");
+	addVec.push_back("aa");
+	addVec.push_back("aaa");
+	REQUIRE(b.add(addVec)); //should be able to add because is unique
+	REQUIRE(!b.add(addVec)); //should not be able to add because not unique
+	vector<string> addVec2 = {"b", "bb", "1"};
+	REQUIRE(!b.add(addVec2)); //contains invalid strings
+	vector<string> addVec3 = {"b", "bb", "a"};
+	REQUIRE(!b.add(addVec3)); //contains repeats
+	REQUIRE(b.add("aaaa")); //is unique
+	REQUIRE(!b.add("AAA")); //is not unique
+	REQUIRE(b.number() == 16);
+}
+
+TEST_CASE("testing RFP", "[FindPalindrome]") {
+	FindPalindrome b;
+    vector<string> addVec;
+    addVec.push_back("sit");
+    addVec.push_back("on");
+    addVec.push_back("a");
+    addVec.push_back("pan");
+    addVec.push_back("potato");
+    addVec.push_back("otis");
+    b.add(addVec);
+    vector<vector<string> > solutions;
+    solutions = b.toVector();
+	vector<vector<string> > actualSolution = {{"sit", "on", "a", "potato", "pan", "otis"}};
+	REQUIRE(solutions == actualSolution);
+	REQUIRE(b.number() == 1);
+	b.clear();
+	REQUIRE(b.number() == 0);
 }
 
--- projects/project2/test.cpp
+++ projects/project2/test.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include "FindPalindrome.cpp"
 using namespace std;
 int main() {
-    string temp = "abcdef1234!*&@(*!Y@H!I@HJUB!IUSYH)";
-    for (int i = 0; i < temp.length(); i++) {
-        cout << temp[i] << " " << isalpha(temp[i]) << endl;
+    FindPalindrome b;
+    vector<string> addVec;
+    addVec.push_back("sit");
+    addVec.push_back("on");
+    addVec.push_back("a");
+    addVec.push_back("potato");
+    addVec.push_back("pan");
+    addVec.push_back("otis");
+    b.add(addVec);
+    vector<vector<string> > solutions;
+    solutions = b.toVector();
+    for (int i = 0; i < solutions.size(); i++) {
+        cout << i << " ";
+        for (int j = 0; j < solutions[i].size(); j++) {
+            cout << solutions[i][j] << " ";
+        }
+        cout << "\n";
     }
+
     return 0;
 }
